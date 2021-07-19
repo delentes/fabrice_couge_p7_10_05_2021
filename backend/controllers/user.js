895c9ba40af7a35e3d4f72e63f4d4a3be933eb19
delentes <delentes03@hotmail.com>
2021-07-19 09:41:50 +0200

    user bdd

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -5,8 +5,8 @@ const mysql = require('mysql')
 const connection = mysql.createConnection({
     host: 'localhost',
     port: '3306',
-    user: 'root',
-    password: '',
+    user: 'admin',
+    password: 'admin',
     database: 'groupomania',
 });
 
